c541c1bf89eebe785b08dd0f3d1784994f280adc
Igor Grebenarovic <igor.grebenarovic@florencehc.com>
2020-04-01 15:33:50 +0200

    Added scripts for labels, monitor reviews and signatures

--- scripts/labels-viewable-entities.js
+++ scripts/labels-viewable-entities.js
@@ -1,17 +1,13 @@
 /**
  * TO BE RAN IN MONGO SHELL
- * collectionName should be one of:
- *  report-approvals
- *  report-tags
- *  report-tasks ...
+ * collectionName should be only report-labels
+ *
+ * null value from the end result in folders array should be removed
  */
 
-var collectionName = 'report-approvals';
+var collectionName = 'report-labels';
 
 var res = db.getCollection(collectionName).aggregate([
-    {
-        $unwind: { path: '$lineage' }
-    },
     {
         $sample: { size: 2000 }
     },
@@ -20,8 +16,13 @@ var res = db.getCollection(collectionName).aggregate([
             _id: 0,
             teamId: 1,
             binderId: 1,
-            documentId: 1,
-            lineage: 1
+            folderId: {
+                $cond: {
+                    if: { $eq: ['$objectType', 'folder'] },
+                    then: '$objectId',
+                    else: null
+                }
+            }
         }
     },
     {
@@ -29,8 +30,7 @@ var res = db.getCollection(collectionName).aggregate([
             _id: null,
             teams: { $addToSet: '$teamId' },
             binders: { $addToSet: '$binderId' },
-            folders: { $addToSet: '$lineage' },
-            documents: { $addToSet: '$documentId' },
+            folders:  { $addToSet: '$folderId' },
         }
     }
 ]).toArray();
@@ -39,7 +39,6 @@ var viewableEntities = {
     teams: [res[0].teams[0]],
     binders: res[0].binders,
     folders: res[0].folders,
-    documents: res[0].documents
 }
 
 printjson(viewableEntities);
